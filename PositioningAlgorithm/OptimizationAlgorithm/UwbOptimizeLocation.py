44bd9c09bc06e1fedb5c13b7cb22900c317077da
steve <551619855@qq.com>
2018-03-18 18:47:42 +0800

    save data.

--- PositioningAlgorithm/OptimizationAlgorithm/UwbOptimizeLocation.py
+++ PositioningAlgorithm/OptimizationAlgorithm/UwbOptimizeLocation.py
@@ -74,6 +74,7 @@ class UwbOptimizeLocation:
         return result.x, result.fun
 
 
+
 if __name__ == '__main__':
     dir_name = '/home/steve/Data/XsensUwb/MTI700/0002/'
     beacon_data = np.loadtxt(dir_name + 'beaconset_no_mac.csv', delimiter=',')
