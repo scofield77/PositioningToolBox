1c3b2ffc5f6e6c7ca22648ec28cd46e32510bd10
steve <551619855@qq.com>
2018-04-06 10:28:48 +0800

    use new process data.

--- AlgorithmTool/pyqtgraph_test.py
+++ AlgorithmTool/pyqtgraph_test.py
@@ -1,5 +1,5 @@
 # -*- coding:utf-8 -*-
-# Created by steve @ 18-3-26 下午8:31
+# Created by steve @ 18-3-31 下午1:47
 '''
                    _ooOoo_ 
                   o8888888o 
@@ -22,25 +22,31 @@
 ^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^^ 
          佛祖保佑       永无BUG 
 '''
-from numpy import pi, sin, cos, mgrid
 
-dphi, dtheta = pi / 250.0, pi / 250.0
-[phi, theta] = mgrid[0:pi + dphi * 1.5:dphi, 0:2 * pi + dtheta * 1.5:dtheta]
-m0 = 4
-m1 = 3
-m2 = 2
-m3 = 3
-m4 = 6
-m5 = 2
-m6 = 6
-m7 = 4
-r = sin(m0 * phi) ** m1 + cos(m2 * phi) ** m3 + sin(m4 * theta) ** m5 + cos(m6 * theta) ** m7
-x = r * sin(phi) * cos(theta)
-y = r * cos(phi)
-z = r * sin(phi) * sin(theta)
-
-# View it.
-from mayavi import mlab
-
-s = mlab.mesh(x, y, z)
-mlab.show()
+import pyqtgraph.examples
+pyqtgraph.examples.run()
+# ## build a QApplication before building other widgets
+# import pyqtgraph as pg
+# pg.mkQApp()
+#
+# ## make a widget for displaying 3D objects
+# import pyqtgraph.opengl as gl
+# view = gl.GLViewWidget()
+# view.show()
+#
+# ## create three grids, add each to the view
+# xgrid = gl.GLGridItem()
+# ygrid = gl.GLGridItem()
+# zgrid = gl.GLGridItem()
+# view.addItem(xgrid)
+# view.addItem(ygrid)
+# view.addItem(zgrid)
+#
+# ## rotate x and y grids to face the correct direction
+# xgrid.rotate(90, 0, 1, 0)
+# ygrid.rotate(90, 1, 0, 0)
+#
+# ## scale each grid differently
+# xgrid.scale(0.2, 0.1, 0.1)
+# ygrid.scale(0.2, 0.1, 0.1)
+# zgrid.scale(0.1, 0.2, 0.1)
