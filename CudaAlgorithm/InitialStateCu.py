483f81871b8319c159b11e10c7ed4aa154ba60df
steve <551619855@qq.com>
2018-04-12 21:48:09 +0800

    cuda particle filter

--- CudaAlgorithm/InitialStateCu.py
+++ CudaAlgorithm/InitialStateCu.py
@@ -105,50 +105,52 @@ def sample(q_array, input, sigma, rng):
 
     euler = cuda.local.array(shape=(3), dtype=float32)
 
-    for i in range(euler.shape[0]):
-        euler[i] = input[i] + (xoroshiro128p_uniform_float32(rng, pos) - 0.5)
-    # Theta = cuda.shared.array([4, 4], dtype=np.float32)
-    Theta = cuda.local.array(shape=(4, 4), dtype=float32)
+    if pos < q_array.shape[1]:
+        for i in range(euler.shape[0]):
+            euler[i] = input[i] + (xoroshiro128p_uniform_float32(rng, pos) - 0.5)
+        # Theta = cuda.shared.array([4, 4], dtype=np.float32)
+        Theta = cuda.local.array(shape=(4, 4), dtype=float32)
 
-    delta_euler = (euler[0] * euler[0] + euler[1] * euler[1] + euler[2] + euler[2])
-    delta_euler = math.sqrt(delta_euler)
+        delta_euler = (euler[0] * euler[0] + euler[1] * euler[1] + euler[2] + euler[2])
+        delta_euler = math.sqrt(delta_euler)
 
-    c = math.cos(delta_euler / 2.0)
-    sdiv = math.sin(delta_euler / 2.0) / delta_euler
+        c = math.cos(delta_euler / 2.0)
+        sdiv = math.sin(delta_euler / 2.0) / delta_euler
 
-    Theta[0, 0] = c
-    Theta[0, 1] = -euler[0] * sdiv
-    Theta[0, 2] = -euler[1] * sdiv
-    Theta[0, 3] = -euler[2] * sdiv
+        Theta[0, 0] = c
+        Theta[0, 1] = -euler[0] * sdiv
+        Theta[0, 2] = -euler[1] * sdiv
+        Theta[0, 3] = -euler[2] * sdiv
 
-    Theta[1, 0] = euler[0] * sdiv
-    Theta[1, 1] = c
-    Theta[1, 2] = euler[2] * sdiv
-    Theta[1, 3] = -euler[1] * sdiv
+        Theta[1, 0] = euler[0] * sdiv
+        Theta[1, 1] = c
+        Theta[1, 2] = euler[2] * sdiv
+        Theta[1, 3] = -euler[1] * sdiv
 
-    Theta[2, 0] = euler[1] * sdiv
-    Theta[2, 1] = -euler[2] * sdiv
-    Theta[2, 2] = c
-    Theta[2, 3] = euler[0] * sdiv
+        Theta[2, 0] = euler[1] * sdiv
+        Theta[2, 1] = -euler[2] * sdiv
+        Theta[2, 2] = c
+        Theta[2, 3] = euler[0] * sdiv
 
-    Theta[3, 0] = euler[2] * sdiv
-    Theta[3, 1] = euler[1] * sdiv
-    Theta[3, 2] = -euler[0] * sdiv
-    Theta[3, 3] = c
+        Theta[3, 0] = euler[2] * sdiv
+        Theta[3, 1] = euler[1] * sdiv
+        Theta[3, 2] = -euler[0] * sdiv
+        Theta[3, 3] = c
 
-    tq = cuda.local.array(4, dtype=float32)
-    tq = q_array[:,pos]
-    norm_new_q = 0.0
-    for i in range(4):
-        q_array[i, pos] = 0.0
+        tq = cuda.local.array(4, dtype=float32)
+        tq = q_array[:,pos]
+        norm_new_q = 0.0
+        for i in range(4):
+            q_array[i, pos] = 0.0
 
-        for j in range(4):
-            q_array[i, pos] += Theta[i, j] * tq[j]
-        norm_new_q += q_array[i, pos] * q_array[i, pos]
-    norm_new_q = math.sqrt(norm_new_q)
+            for j in range(4):
+                q_array[i, pos] += Theta[i, j] * tq[j]
+            norm_new_q += q_array[i, pos] * q_array[i, pos]
+        norm_new_q = math.sqrt(norm_new_q)
 
-    for i in range(4):
-        q_array[i, pos] = q_array[i, pos] / norm_new_q
+        norm_new_q = 0.5
+        for i in range(3):
+            q_array[i, pos] = q_array[i, pos] / norm_new_q
 
 
 if __name__ == '__main__':
@@ -170,15 +172,19 @@ if __name__ == '__main__':
     rng_states = create_xoroshiro128p_states(block_num * thread_pre_block, seed=1)
 
     q_state = cuda.device_array([4, particle_num], dtype=np.float32)
+
     initial_unit_q[block_num, thread_pre_block](q_state)
+    print('after initial')
 
     input_array = cuda.device_array(input_num - 3, dtype=np.float32)
     input_array = cuda.to_device(np.zeros(input_num - 3))
 
+    print('before create euler')
     euler_array = cuda.device_array([3, particle_num], dtype=np.float32)
 
-    sample[block_num,particle_num](q_state, input_array, 0.0, rng_states)
-
+    print('befoer sample')
+    sample[block_num,thread_pre_block](q_state, input_array, 0.0, rng_states)
+    print('after sample')
     # print(q_state.to_host())
     q_state_host = np.empty(shape=q_state.shape, dtype=q_state.dtype)
     # q_state_host = q_state.to_host()
