9e825fce37263149b4fbdf8ffbc16a3a75c7f68a
steve <551619855@qq.com>
2018-05-02 21:10:50 +0800

    test robust algorith in new data, bad result.

--- PositioningAlgorithm/tester/UwbAnalysisTest.py
+++ PositioningAlgorithm/tester/UwbAnalysisTest.py
@@ -23,9 +23,9 @@
          佛祖保佑       永无BUG 
 '''
 
-
 import numpy as np
 import scipy as sp
+from scipy import interpolate
 import matplotlib.pyplot as plt
 
 from numba import jit
@@ -34,6 +34,7 @@ import matplotlib
 
 import time
 
+from PositioningAlgorithm.OptimizationAlgorithm.UwbOptimizeLocation import UwbOptimizeLocation
 
 if __name__ == '__main__':
     matplotlib.use('Qt5Agg')
@@ -43,22 +44,49 @@ if __name__ == '__main__':
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
     dir_name = '/home/steve/Data/NewFusingLocationData/0032/'
 
-
     # uwb_data = np.loadtxt(dir_name + 'uwb_result.csv', delimiter=',')
     # beacon_set = np.loadtxt(dir_name + 'beaconSet.csv', delimiter=',')
     uwb_data = np.loadtxt(dir_name + 'uwb_data.csv', delimiter=',')
     beacon_set = np.loadtxt(dir_name + 'beaconset_no_mac.csv', delimiter=',')
 
-    ref_trace = np.loadtxt(dir_name+'ref_trace.csv',delimiter=',')
+    uol = UwbOptimizeLocation(beacon_set)
+    uwb_trace = np.zeros([uwb_data.shape[0], 3])
+    uwb_opt_res = np.zeros([uwb_data.shape[0]])
+    for i in range(uwb_data.shape[0]):
+        if i is 0:
+            uwb_trace[i, :], uwb_opt_res[i] = \
+                uol.positioning_fucntion((0, 0, 0),
+                                         uwb_data[i, 1:])
+        else:
+            uwb_trace[i, :], uwb_opt_res[i] = \
+                uol.positioning_fucntion(uwb_trace[i - 1, :],
+                                         uwb_data[i, 1:])
+
+    ref_trace = np.loadtxt(dir_name + 'ref_trace.csv', delimiter=',')
 
     plt.figure()
-    plt.plot(ref_trace[:,0],ref_trace[:,1],'r-+')
+    plt.plot(ref_trace[:, 1], ref_trace[:, 2], '-+', label='ref')
+    plt.plot(uwb_trace[:, 0], uwb_trace[:, 1], '-+', label='uwb')
     plt.grid()
 
+    # ref_t_f = interpolate.interp2d(ref_trace[:,0],ref_trace[:,1],ref_trace[:,2])
+    ref_t_fx = interpolate.interp1d(ref_trace[:,0],ref_trace[:,1])
+    ref_t_fy = interpolate.interp1d(ref_trace[:,0],ref_trace[:,2])
+
+    ref_t_trace = np.zeros(shape=(uwb_data.shape[0],2))
+    ref_t_trace[:,0] = ref_t_fx(uwb_data[:,0])
+    ref_t_trace[:,1] = ref_t_fy(uwb_data[:,0])
+
+    plt.figure()
+    plt.plot(ref_t_trace[:,0],ref_t_trace[:,1])
+    plt.grid()
+    print(ref_t_trace.shape,uwb_trace.shape)
+
+
+
+    # plt.figure()
+    # plt.title('error')
+    # plt.plot(np.linalg.norm(ref_trace-uwb_trace,axis=0),)
+    # plt.grid()
 
     plt.show()
-
-
-
-
-
