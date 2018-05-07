8a0a8181034d1f945c0569743d41e6a113bfcd2f
steve <551619855@qq.com>
2018-05-07 15:04:06 +0800

    IEKF with extended EKF for UWb measurements obtained a good result. (named DEKF).

--- PositioningAlgorithm/tester/FootImuUwbTest.py
+++ PositioningAlgorithm/tester/FootImuUwbTest.py
@@ -59,7 +59,7 @@ if __name__ == '__main__':
     start_time = time.time()
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0033/'
+    dir_name = '/home/steve/Data/NewFusingLocationData/0032/'
 
     # imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
     imu_data = np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=',')
