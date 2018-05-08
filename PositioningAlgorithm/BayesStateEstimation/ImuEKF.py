7acd5e6631a43ddd7e3d4a4135ebda561402b890
steve <551619855@qq.com>
2018-05-08 15:04:11 +0800

    optimized out put.

--- PositioningAlgorithm/BayesStateEstimation/ImuEKF.py
+++ PositioningAlgorithm/BayesStateEstimation/ImuEKF.py
@@ -398,7 +398,7 @@ class ImuEKFComplex:
                     else:
                         # print('corrected Rk')
                         # mask[index[i]] = ka_squard / gamma * 1.0
-                        mask[index[i]] = 0.5#ka_squard/gamma
+                        # mask[index[i]] = 0.5#ka_squard/gamma
                         Rk[index[i], index[i]] = gamma / ka_squard * Rk[index[i], index[i]]
                         # mask[index[i]] = 1.0 / gamma
                     i = index.shape[0] + 1
