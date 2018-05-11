cd3cb7cf7e63c2ee30cd0080cf85e3756ac0bfb0
steve <551619855@qq.com>
2018-05-11 20:20:26 +0800

    calculated unknow beacon's pose.

--- PositioningAlgorithm/tester/FootImuUwbTest.py
+++ PositioningAlgorithm/tester/FootImuUwbTest.py
@@ -145,7 +145,8 @@ if __name__ == '__main__':
     #                                  ref_trace[ti, 1] - ref_trace[0, 1]) - 10.0 * np.pi / 180.0  # 35
     # initial_orientation = math.atan2(ref_trace[ti, 2] - ref_trace[0, 2],
     #                                  ref_trace[ti, 1] - ref_trace[0, 1]) + 150.0 * np.pi / 180.0  # 32
-    initial_orientation = 80.0 * np.pi / 180.0#38-45
+    # initial_orientation = 80.0 * np.pi / 180.0#38-45
+    initial_orientation = 60.0 * np.pi / 180.0#38-45
 
     #  initial_orientation = 200.0 / 180.0 * np.pi
 
