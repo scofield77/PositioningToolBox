55f90a80dda4916ef51fa8c223aa418e65834b9d
steve <551619855@qq.com>
2018-05-12 21:55:04 +0800

    refer evaluation done.

--- AlgorithmTool/ReferTraceEvaluateTools.py
+++ AlgorithmTool/ReferTraceEvaluateTools.py
@@ -46,20 +46,29 @@ class Refscor:
                 self.relution = (self.map_range[0,1]-self.map_range[0,0])/float(self.score_data.shape[0])
                 # print(self.map_range)
                 print(self.relution)
-    # def eval_point2d(self,point):
-        # pos_x =
+        self.max_score = np.max(self.score_data)
+    def eval_point2d(self,point):
+        point = point.reshape(-1)
+        pos_x = int((point[0]-self.map_range[0,0])/self.relution)
+        pos_y = int((point[1]-self.map_range[1,0])/self.relution)
+        return self.score_data[pos_x,pos_y]
 
 
 
 
 if __name__ == '__main__':
-    dir_name = '/home/steve/Data/NewFusingLocationData/0039/'
+    dir_name = '/home/steve/Data/NewFusingLocationData/0038/'
     ref_trace  = np.loadtxt(dir_name+'ref_trace.csv',delimiter=',')
 
     # print(ref_trace)
 
     rs = Refscor(dir_name)
+    score = np.zeros(shape=(ref_trace.shape[0]))
+    for i in range(ref_trace.shape[0]):
+        score[i] = rs.eval_point2d(ref_trace[i,1:3])
 
 
     plt.figure()
-    plt.plot()
+    plt.plot(score)
+    plt.grid()
+    plt.show()
