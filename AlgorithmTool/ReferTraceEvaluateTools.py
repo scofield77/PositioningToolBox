71122b045c046f2ffbcc863f3e0f6efee5a92584
steve <551619855@qq.com>
2018-05-13 10:12:52 +0800

    refer evaluation done.

--- AlgorithmTool/ReferTraceEvaluateTools.py
+++ AlgorithmTool/ReferTraceEvaluateTools.py
@@ -23,11 +23,10 @@
          佛祖保佑       永无BUG 
 '''
 
-import  numpy as np
+import numpy as np
 import scipy as sp
 
 import matplotlib.pyplot as plt
-
 
 import os
 import re
@@ -38,36 +37,34 @@ class Refscor:
         for name in os.listdir(file_dir):
             rem = re.compile('[0-9\.\-]{1,100}npy$')
             if rem.match(name):
-                self.score_data = np.load(dir_name+name)
+                self.score_data = np.load(dir_name + name)
                 name = name.split('.npy')[0]
                 self.map_range = np.asarray(
                     ([float(x) for x in name.split('-')])
-                ).reshape(2,2)
-                self.relution = (self.map_range[0,1]-self.map_range[0,0])/float(self.score_data.shape[0])
+                ).reshape(2, 2)
+                self.relution = (self.map_range[0, 1] - self.map_range[0, 0]) / float(self.score_data.shape[0])
                 # print(self.map_range)
                 print(self.relution)
         self.max_score = np.max(self.score_data)
-    def eval_point2d(self,point):
+
+    def eval_point2d(self, point):
         point = point.reshape(-1)
-        pos_x = int((point[0]-self.map_range[0,0])/self.relution)
-        pos_y = int((point[1]-self.map_range[1,0])/self.relution)
-        return self.score_data[pos_x,pos_y]
-
-
+        pos_x = int((point[0] - self.map_range[0, 0]) / self.relution)
+        pos_y = int((point[1] - self.map_range[1, 0]) / self.relution)
+        return self.score_data[pos_x, pos_y]
 
 
 if __name__ == '__main__':
     dir_name = '/home/steve/Data/NewFusingLocationData/0038/'
-    ref_trace  = np.loadtxt(dir_name+'ref_trace.csv',delimiter=',')
+    ref_trace = np.loadtxt(dir_name + 'ref_trace.csv', delimiter=',')
 
     # print(ref_trace)
 
     rs = Refscor(dir_name)
     score = np.zeros(shape=(ref_trace.shape[0]))
     for i in range(ref_trace.shape[0]):
-        score[i] = rs.eval_point2d(ref_trace[i,1:3])
-    print(np.mean(score),np.std(score))
-
+        score[i] = rs.eval_point2d(ref_trace[i, 1:3])
+    print(np.mean(score), np.std(score))
 
     plt.figure()
     plt.plot(score)
