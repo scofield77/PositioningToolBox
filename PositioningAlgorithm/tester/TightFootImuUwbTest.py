3a768d348d113c6064bebfe9bb5f8a6d77bf8511
steve <551619855@qq.com>
2018-05-15 15:15:16 +0800

    robust multi

--- PositioningAlgorithm/tester/TightFootImuUwbTest.py
+++ PositioningAlgorithm/tester/TightFootImuUwbTest.py
@@ -61,7 +61,7 @@ if __name__ == '__main__':
     start_time = time.time()
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0042/'
+    dir_name = '/home/steve/Data/NewFusingLocationData/0036/'
     # dir_name = 'D:/Data/NewFusingLocationData/0033/'
 
     imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
