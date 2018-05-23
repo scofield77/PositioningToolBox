d890063b5f91e21be7f80e9418ef441e9c9eceed
wang stephen <551619855@qq.com>
2018-05-24 07:59:58 +0800

    modified in windows.(algorithm same to document.)_For paper.

--- PositioningAlgorithm/tester/TightFootImuUwbTest.py
+++ PositioningAlgorithm/tester/TightFootImuUwbTest.py
@@ -61,11 +61,11 @@ if __name__ == '__main__':
     start_time = time.time()
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0036/'
-    # dir_name = 'D:/Data/NewFusingLocationData/0040/'
+    # dir_name = '/home/steve/Data/NewFusingLocationData/0036/'
+    dir_name = 'D:/Data/NewFusingLocationData/0036/'
 
-    imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
-    # imu_data = np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=',')
+    # imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
+    imu_data = np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=',')
     # imu_data = np.loadtxt(dir_name + 'HEAD.data', delimiter=',')
     imu_data = imu_data[:, 1:]
     imu_data[:, 1:4] = imu_data[:, 1:4] * 9.81
