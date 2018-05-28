5bf1ed901056697e07f11bde9b33e85d2fade635
steve <551619855@qq.com>
2018-05-28 21:12:59 +0800

    mc ekf better than standard ekf and robust ekf in 0039 and 0040 with 5 4 3 beacon.

--- PositioningAlgorithm/tester/FootImuMCMHTTest.py
+++ PositioningAlgorithm/tester/FootImuMCMHTTest.py
@@ -64,7 +64,7 @@ if __name__ == '__main__':
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
     dir_name = '/home/steve/Data/NewFusingLocationData/0039/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0039/'
+    dir_name = '/home/steve/Data/NewFusingLocationData/0040/'
     # dir_name = 'D:/Data/NewFusingLocationData/0039/'
 
     # imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
@@ -106,9 +106,9 @@ if __name__ == '__main__':
     #     uwb_data[:, uwb_valid[random_index[i]]] *= 0.0
     #     uwb_data[:, uwb_valid[random_index[i]]] -= 10.0
 
-    delet_index = [ 33, 35]  # use 5 beacons
+    # delet_index = [ 33, 35]  # use 5 beacons
     # delet_index = [30, 33, 35]  # use 4 beacons
-    # delet_index = [30, 33, 35, 36]  # use 3 beacons
+    delet_index = [30, 33, 35, 36]  # use 3 beacons
     # delet_index = [30, 31, 33, 34, 35]  # use 2 beacons
     # print('delet index:', type(delet_index), delet_index)
     for i in range(len(delet_index)):
