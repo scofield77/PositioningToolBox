9937316fc07808a2cd126cc44bc803179e3f2fbf
steve <551619855@qq.com>
2018-06-03 16:12:18 +0800

    new ref.

--- VisionTool/ImageSelect.py
+++ VisionTool/ImageSelect.py
@@ -31,11 +31,11 @@ if __name__ == '__main__':
     dir_name = '/media/steve/cb325f68-9e23-45ae-8fc1-0f7c0ee58131/ImageData/100GOPRO/park1/'
     new_dir_name ='/media/steve/cb325f68-9e23-45ae-8fc1-0f7c0ee58131/ImageData/100GOPRO/park1_cut/'
 
-    os.mkdir(new_dir_name)
+    # os.mkdir(new_dir_name)
 
     for file_name in os.listdir(dir_name):
         if 'png' in file_name:
             code = int(file_name.split('.')[0])
 
-            if code % 9 is 0:
+            if code % 3 is 0:
                 shutil.copyfile(dir_name+file_name,new_dir_name+file_name)
