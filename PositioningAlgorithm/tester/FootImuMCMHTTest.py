b57a8b3c885834f5645d62a19b51aaac9402b26d
wang stephen <551619855@qq.com>
2018-06-06 17:07:13 +0800

    some error

--- PositioningAlgorithm/tester/FootImuMCMHTTest.py
+++ PositioningAlgorithm/tester/FootImuMCMHTTest.py
@@ -63,10 +63,10 @@ if __name__ == '__main__':
     start_time = time.time()
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
     # dir_name = '/home/steve/Data/FusingLocationData/0013/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0039/'
+    # dir_name = '/home/steve/Data/NewFusingLocationData/0039/'
     # dir_name = '/home/steve/Data/NewFusingLocationData/0040/'
     # dir_name = 'C:/Data/NewFusingLocationData/0039/'
-    # dir_name = 'D:/Data/NewFusingLocationData/0039/'
+    dir_name = 'D:/Data/NewFusingLocationData/0039/'
 
     ref_score = Refscor(dir_name)
     # imu_data = np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=',')
