5764c3155e8bcd54e317ce552441455211045457
wang stephen <551619855@qq.com>
2018-06-06 20:33:04 +0800

    data analysis.

---
+++ DataProcess/UwbDataAnalysis.py
@@ -0,0 +1,24 @@
+# -*- coding:utf-8 -*-
+# carete by steve at  2018 / 06 / 06　20:28
+
+import numpy as np
+import scipy as sp
+
+import matplotlib.pyplot as plt
+
+
+
+
+if __name__ == '__main__':
+    dir_name = '/home/steve/Data/NewFusingLocationData/0044/'
+    base_dir_name = 'D:/Data/NewFusingLocationData/'
+
+    test_dir_name = base_dir_name+"0047/"
+
+    def plot_static_measurement(dir_name):
+        uwb_data =
+
+
+
+
+
