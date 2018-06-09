a6c29103b3a9a43340be511777ad165e356930cd
wang stephen <551619855@qq.com>
2018-06-09 21:14:49 +0800

    data analysis.

--- PositioningAlgorithm/tester/FootImuMCMHTTest.py
+++ PositioningAlgorithm/tester/FootImuMCMHTTest.py
@@ -390,12 +390,12 @@ if __name__ == '__main__':
                     # rkf.measurement_uwb_mc(np.asarray(uwb_data[uwb_index,1:]),
                     #                        np.ones(1)*0.01,
                     #                        beacon_set, ref_trace)
-                    # kf.measurement_uwb_mc(np.asarray(uwb_data[uwb_index,1:]),
-                    #                        np.ones(1)*0.01,
-                    #                        beacon_set, ref_trace)
-                    orkf.measurement_uwb_mc_itea(np.asarray(uwb_data[uwb_index, 1:]),
-                                                 np.ones(1) * 0.01,
-                                                 beacon_set, ref_trace)
+                    orkf.measurement_uwb_mc(np.asarray(uwb_data[uwb_index,1:]),
+                                           np.ones(1)*0.01,
+                                           beacon_set, ref_trace)
+                    # orkf.measurement_uwb_mc_itea(np.asarray(uwb_data[uwb_index, 1:]),
+                    #                              np.ones(1) * 0.01,
+                    #                              beacon_set, ref_trace)
 
                     tmp_index = 0
                     for k in range(1, uwb_data.shape[1]):
