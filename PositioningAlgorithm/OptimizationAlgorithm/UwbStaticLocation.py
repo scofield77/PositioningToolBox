f6fea124822493edff24765165e6da037978c42e
wang stephen <551619855@qq.com>
2018-06-13 16:32:01 +0800

    compute analysis data.

--- PositioningAlgorithm/OptimizationAlgorithm/UwbStaticLocation.py
+++ PositioningAlgorithm/OptimizationAlgorithm/UwbStaticLocation.py
@@ -36,18 +36,19 @@ class UwbStaticLocation:
                 return u2
             else:
                 return 0.5
+
         # @jit(nopython=True)
-        def compute_error(pose, measurement, beaconset):
+        def compute_error(pose, measurement, beaconset, m_w):
             # print(measurement.shape)
             error_sum = 0
             for i in range(measurement.shape[0]):
                 for j in range(measurement.shape[1]):
-                    if measurement[i, j] > 0.0 and beaconset[j, 0] < 5000.0:
-                        error_sum += rou((measurement[i,j] - np.linalg.norm(pose - beaconset[j, :])) ** 2.0 )/( measurement[i,j] ** 0.5)
+                    if measurement[i, j] > 0.0 and beaconset[j, 0] < 5000.0 and m_w[j] > 0.95:
+                        error_sum += rou((measurement[i, j] - np.linalg.norm(pose - beaconset[j, :])) ** 2.0)# / (
+                                    # measurement[i, j] ** 0.5)
             return error_sum ** 0.5
 
-        return compute_error(pose, self.measurements, self.beacon_set)
-
+        return compute_error(pose, self.measurements, self.beacon_set, self.m_weight)
 
     def positioning_error_function(self, pose):
         '''
@@ -63,19 +64,24 @@ class UwbStaticLocation:
             for i in range(measurement.shape[0]):
                 for j in range(measurement.shape[1]):
                     if measurement[i, j] > 0.0 and beaconset[j, 0] < 5000.0:
-                        error_sum += ((measurement[i,j] - np.linalg.norm(pose - beaconset[j, :])) ** 2.0 )
+                        error_sum += ((measurement[i, j] - np.linalg.norm(pose - beaconset[j, :])) ** 2.0)
             return error_sum ** 0.5
 
         return compute_error(pose, self.measurements, self.beacon_set)
 
-
-
-
     def calculate_position(self, measurement):
         self.measurements = measurement * 1.0
 
-
         self.m_weight = np.zeros(self.measurements.shape[1])
+
+        for i in range(self.measurements.shape[1]):
+            if np.max(self.measurements[:, i]) > 0.0 and self.beacon_set[i,0] < 5000.0:
+                tmp_measurement = self.measurements[:, i] * 1.0
+                valid_number = (tmp_measurement[tmp_measurement > 0.0]).shape[0]
+                print(np.mean(tmp_measurement[tmp_measurement>0.0]),
+                      np.std(tmp_measurement[tmp_measurement>0.0]),
+                      float(tmp_measurement[tmp_measurement>0.0].shape[0])/float(tmp_measurement.shape[0]))
+                self.m_weight[i] = float(tmp_measurement[tmp_measurement>0.0].shape[0])/float(tmp_measurement.shape[0])
 
         initial_pose = np.zeros(3)
         result = minimize(self.positioning_error_function,
