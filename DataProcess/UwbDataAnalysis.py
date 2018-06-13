09af1904826380591aa5e297f7024e202d3663b5
wang stephen <551619855@qq.com>
2018-06-13 20:18:59 +0800

    compute analysis data.

--- DataProcess/UwbDataAnalysis.py
+++ DataProcess/UwbDataAnalysis.py
@@ -14,39 +14,44 @@ if __name__ == '__main__':
     test_dir_name = base_dir_name + "0058/"
 
 
-    def plot_static_measurement(dir_name):
+    def plot_static_measurement(dir_name, p_i, total_num):
         uwb_data = np.loadtxt(dir_name + 'uwb_data.csv', delimiter=',')
         beacon_set = np.loadtxt(dir_name + 'beaconset_no_mac.csv', delimiter=',')
         usl = UwbStaticLocation.UwbStaticLocation(beacon_set)
         pose = usl.calculate_position(uwb_data[:, 1:])
-        plt.figure(int(dir_name.split('/')[-2]))
+        # plt.figure(int(dir_name.split('/')[-2]))
+        plt.figure(13)
+
         valid_count = 0
         for i in range(beacon_set.shape[0]):
-            if beacon_set[i,0] < 1000.0 and np.max(uwb_data[:,i+1])>0.0:
-                valid_count+=1
+            if beacon_set[i, 0] < 1000.0 and np.max(uwb_data[:, i + 1]) > 0.0:
+                valid_count += 1
         valid_index = 0
         for i in range(beacon_set.shape[0]):
             if beacon_set[i, 0] < 1000.0 and np.max(uwb_data[:, i + 1]) > 0.0:
-                valid_index+=1
-                plt.subplot(100 * valid_count+10+valid_index)
-                plt.title('label:' + str(i))
+                print(valid_index, p_i, valid_index)
+                plt.subplot(100 * valid_count + 10 * total_num + valid_index + ((p_i - 1) * 6) + 1)
+                # plt.subplot(100*)
+                plt.title(str(i - 29))
 
                 tmp_uwb_data = uwb_data[:, i + 1] * 1.0
                 plt.hist(tmp_uwb_data[tmp_uwb_data > 0.0],
                          label=str(tmp_uwb_data[tmp_uwb_data > 0].shape[0]) + '/' + str(tmp_uwb_data.shape[0]))
-                plt.axvline(np.linalg.norm(pose-beacon_set[i,:]),color='k',linestyle='dashed',linewidth=1)
+                plt.axvline(np.linalg.norm(pose - beacon_set[i, :]), color='k', linestyle='dashed', linewidth=1)
+                plt.tight_layout()
 
+                valid_index += 1
                 plt.legend()
         plt.figure(111)
         plt.title('beacon')
         i_list = list()
         for i in range(beacon_set.shape[0]):
             if beacon_set[i, 0] < 1000.0 and np.max(uwb_data[:, i + 1]) > 0.0:
-                plt.text(beacon_set[i, 0], beacon_set[i, 1],  str(i-29))
+                plt.text(beacon_set[i, 0], beacon_set[i, 1], str(i - 29))
                 i_list.append(i)
         plt.plot(beacon_set[i_list, 0], beacon_set[i_list, 1], 'r*')
 
-        plt.plot(pose[0], pose[1], 'r+')
+        plt.plot(pose[0], pose[1], 'b+')
 
         plt.text(pose[0], pose[1], 'p' + dir_name.split('/')[-2])
 
@@ -55,7 +60,11 @@ if __name__ == '__main__':
 
 
     # plot_static_measurement(test_dir_name)
-    for i in range(53,62):
-        plot_static_measurement(base_dir_name + '%04d/' % (i))
+
+    start_i = 53
+    total_num = 3
+    for i in range(start_i, total_num + start_i):
+        plot_static_measurement(base_dir_name + '%04d/' % (i), i - start_i + 1, total_num)
+
 
     plt.show()
