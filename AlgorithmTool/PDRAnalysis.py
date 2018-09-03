79e651482d9d0aab7e8f78e13727e7749c1ded63
steve <551619855@qq.com>
2018-09-03 17:51:22 +0800

    pf cuda test.

--- AlgorithmTool/PDRAnalysis.py
+++ AlgorithmTool/PDRAnalysis.py
@@ -23,8 +23,6 @@
          佛祖保佑       永无BUG 
 '''
 
-
-
 import numpy as np
 import scipy as sp
 from numba import jit
@@ -60,7 +58,7 @@ if __name__ == '__main__':
     # matplotlib.rcParams['toolbar'] = 'toolmanager'
     start_time = time.time()
     # dir_name = '/home/steve/Data/FusingLocationData/0017/'
-    dir_name = '/home/steve/Data/NewFusingLocationData/0033/'
+    dir_name = '/home/steve/Data/NewFusingLocationData/0035/'
 
     # dir_name = 'D:\\NewFusingLocationData\\0035\\'
 
@@ -71,42 +69,39 @@ if __name__ == '__main__':
     left_imu_data[:, 1:4] = left_imu_data[:, 1:4] * 9.81
     left_imu_data[:, 4:7] = left_imu_data[:, 4:7] * (np.pi / 180.0)
 
-    right_imu_data = right_imu_data[:,1:]
-    right_imu_data[:,1:4] = right_imu_data[:,1:4] * 9.81
-    right_imu_data[:,4:7] = right_imu_data[:,4:7]  * (np.pi/180.0)
+    right_imu_data = right_imu_data[:, 1:]
+    right_imu_data[:, 1:4] = right_imu_data[:, 1:4] * 9.81
+    right_imu_data[:, 4:7] = right_imu_data[:, 4:7] * (np.pi / 180.0)
 
-    t_phone_imu_data = np.loadtxt(dir_name + 'HAND_SMARTPHONE_IMU.data',delimiter=',')
+    t_phone_imu_data = np.loadtxt(dir_name + 'HAND_SMARTPHONE_IMU.data', delimiter=',')
     # phone_imu_data = phone_imu_data[:,1:]
-    phone_imu_data = np.zeros([t_phone_imu_data.shape[0],t_phone_imu_data.shape[1]-1])
-    phone_imu_data[:,0] = (t_phone_imu_data[:,0]-t_phone_imu_data[0,0])*1e-6 + t_phone_imu_data[0,1]#left_imu_data[0,0]
-    phone_imu_data[:,1:] = t_phone_imu_data[:,2:]
+    phone_imu_data = np.zeros([t_phone_imu_data.shape[0], t_phone_imu_data.shape[1] - 1])
+    phone_imu_data[:, 0] = (t_phone_imu_data[:, 0] - t_phone_imu_data[0, 0]) * 1e-6 + t_phone_imu_data[
+        0, 1]  # left_imu_data[0,0]
+    phone_imu_data[:, 1:] = t_phone_imu_data[:, 2:]
 
-    print(phone_imu_data[-1,0]-phone_imu_data[0,0])
-    print(left_imu_data[-1,0]-left_imu_data[0,0])
+    print(phone_imu_data[-1, 0] - phone_imu_data[0, 0])
+    print(left_imu_data[-1, 0] - left_imu_data[0, 0])
 
-    l_zv = GLRT_Detector(left_imu_data[:,1:7],sigma_a=1.0,sigma_g=1.0 * np.pi/180.0,
-                         gamma=200,
+    l_zv = GLRT_Detector(left_imu_data[:, 1:7], sigma_a=1.0, sigma_g=1.0 * np.pi / 180.0,
+                         gamma=300,
                          gravity=9.8,
                          time_Window_size=5)
-    r_zv = GLRT_Detector(right_imu_data[:,1:7],sigma_a=1.0,sigma_g=1.0 * np.pi/180.0,
-                         gamma=200,
+    r_zv = GLRT_Detector(right_imu_data[:, 1:7], sigma_a=1.0, sigma_g=1.0 * np.pi / 180.0,
+                         gamma=300,
                          gravity=9.8,
                          time_Window_size=5)
-
 
     plt.figure()
     plt.subplot(211)
     # for i in range(1,4):
     #     plt.plot(phone_imu_data[:,0],phone_imu_data[:,i],'-+')
-    plt.plot(phone_imu_data[:,0],np.linalg.norm(phone_imu_data[:,1:4],axis=1)/10.0)
-    plt.plot(left_imu_data[:,0],l_zv,'--y')
-    plt.plot(right_imu_data[:,0],r_zv,'--r')
+    plt.plot(phone_imu_data[:, 0], np.linalg.norm(phone_imu_data[:, 1:4], axis=1) / 10.0)
+    plt.plot(left_imu_data[:, 0], l_zv, '--y')
+    plt.plot(right_imu_data[:, 0], r_zv, '--r')
 
     plt.subplot(212)
-    for i in range(1,4):
-        plt.plot(left_imu_data[:,0],left_imu_data[:,i])
+    for i in range(1, 4):
+        plt.plot(left_imu_data[:, 0], left_imu_data[:, i])
 
     plt.show()
-
-
-
