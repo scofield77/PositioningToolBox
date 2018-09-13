05fa6333fab658426f96f6c7bc0a74527d5a86f1
steve <551619855@qq.com>
2018-09-13 09:05:00 +0800

    ...

--- AlgorithmExperiments/PDR_StepDetectorExperiment.py
+++ AlgorithmExperiments/PDR_StepDetectorExperiment.py
@@ -25,19 +25,18 @@
 
 import matplotlib.pyplot as plt
 
-import  numpy as np
+import numpy as np
 from numpy.linalg import *
 import scipy as sp
 
 from AlgorithmTool.ImuTools import *
-
 
 if __name__ == '__main__':
     data = np.loadtxt('/home/steve/Data/phoneData/PDRUWBBLEMini/0006/SMARTPHONE3_IMU.data', delimiter=',')
     # step_detector = StepDetector(1.0, 0.8)
     # step_estimator = StepLengthEstimatorV()
 
-    t = data[:, 0]-data[0,0]
+    t = data[:, 0] - data[0, 0]
     data = data[:, 1:]
     data[:, 0] = t * 1.0
 
@@ -58,35 +57,35 @@ if __name__ == '__main__':
     ori[:, 1:] = data[:, 11:14]
 
     # imu_plot_aux()
-    imu_plot_aux(acc,'acc')
-
+    imu_plot_aux(acc, 'acc')
 
     # def peak_and_valley_detector(acc,rotation):
     flag_array = np.zeros(acc.shape[0])
-    low_pass_array = np.zeros(acc.shape[0])
-    low_pass_array[0] = np.linalg.norm(acc[0,1:])
-    low_pass_alpha = 0.05
+    # low_pass_array[0] = np.linalg.norm(acc[0,1:])
+    low_pass_alpha_list = {0.002, 0.01, 0.5}
+    low_pass_array = np.zeros([acc.shape[0], len(low_pass_alpha_list)])
+    low_pass_array[0, :] = 0.0 + np.linalg.norm(acc[0, 1:])
 
+    for i in range(1, flag_array.shape[0] - 1):
+        for low_pass_alpha, j in enumerate(low_pass_alpha_list):
+            low_pass_array[i, j] = (1.0 - low_pass_alpha) * low_pass_array[i - 1] + (low_pass_alpha) * np.linalg.norm(
+                acc[i, 1:])
 
-    for i in range(1,flag_array.shape[0]-1):
-        low_pass_array[i] = (1.0-low_pass_alpha) * low_pass_array[i-1] + (low_pass_alpha) * np.linalg.norm(acc[i,1:])
+        if np.linalg.norm(acc[i, 1:]) > max(np.linalg.norm(acc[i - 1, 1:]), np.linalg.norm(acc[i + 1, 1:])):
+            flag_array[i] = np.linalg.norm(acc[i, 1:])
+        elif np.linalg.norm(acc[i, 1:]) < min(np.linalg.norm(acc[i - 1, 1:]), np.linalg.norm(acc[i + 1, 1:])):
+            flag_array[i] = np.linalg.norm(acc[i, 1:])
 
-        if np.linalg.norm(acc[i,1:]) > max(np.linalg.norm(acc[i-1,1:]),np.linalg.norm(acc[i+1,1:])):
-            flag_array[i] = np.linalg.norm(acc[i,1:])
-        elif np.linalg.norm(acc[i,1:]) < min(np.linalg.norm(acc[i-1,1:]),np.linalg.norm(acc[i+1,1:])):
-            flag_array[i] = np.linalg.norm(acc[i,1:])
-
-    plt.plot(acc[:,0],flag_array,'+')
+    plt.plot(acc[:, 0], flag_array, '+')
     # plt.plot(mag[:,0],np.linalg.norm(mag[:,1:],axis=1)/5.0)
-    plt.plot(gyr[:,0],np.linalg.norm(gyr[:,1:],axis=1)/2.5)
-    plt.plot(acc[:,0],low_pass_array,'--',label='low_pass')
-
+    plt.plot(gyr[:, 0], np.linalg.norm(gyr[:, 1:], axis=1) / 2.5)
     plt.legend()
 
-
-
-
-
+    # plt.plot(acc[:,0],low_pass_array,'--',label='low_pass')
+    plt.figure()
+    for j in range(len(low_pass_alpha)):
+        plt.plot(low_pass_array[j,:],label=str(j))
+    plt.legend()
 
 
     plt.show()
