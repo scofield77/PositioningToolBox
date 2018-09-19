5b4515511496cffff6fb316cc634562481818568
steve <551619855@qq.com>
2018-09-19 17:36:58 +0800

    multi-parameter step detector could provide an acceptable result.

--- PDRTool/DataChronicAnalysis.py
+++ PDRTool/DataChronicAnalysis.py
@@ -29,16 +29,39 @@ import matplotlib.pyplot as plt
 
 from AlgorithmTool.ImuTools import imu_plot_aux
 
+
+def imu_data_preprocess(imu_data):
+    '''
+
+    :param imu_data:
+    :return: time(s),acc x-y-z(m/s/s), gyr x-y-z (rad/s), mag(?)
+    '''
+    imu_data = imu_data[:, 1:]
+    imu_data[:, 1:4] = imu_data[:, 1:4] * 9.81
+    imu_data[:, 4:7] = imu_data[:, 4:7] * (np.pi / 180.0)
+
+    return imu_data * 1.0  #
+
+
 if __name__ == '__main__':
     dir_name = '/home/steve/Data/PDR/0001/'
 
-    left_imu = np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=',')
+    left_imu = imu_data_preprocess(np.loadtxt(dir_name + 'LEFT_FOOT.data', delimiter=','))
+    right_imu = imu_data_preprocess(np.loadtxt(dir_name + 'RIGHT_FOOT.data', delimiter=','))
+    head_imu = imu_data_preprocess(np.loadtxt(dir_name + 'HEAD.data', delimiter=','))
+    # phone_imu =imu_data_preprocess(np.)
+    phone_imu = np.loadtxt(dir_name + 'SMARTPHONE2_IMU.data', delimiter=',')[:,1:]
 
-    left_imu = left_imu[:, 1:]
 
-    plt.figure()
+    # left_imu = left_imu[:, 1:]
+
+    # plt.figure()
     # plt.plot(left_imu[:,1:4],'acc')
-    imu_plot_aux(left_imu[:, 1:4], time=left_imu[:, 0], title_name='acc')
-    imu_plot_aux(left_imu[:, 4:7], time=left_imu[:, 0], title_name='gyr')
+    # imu_plot_aux(left_imu[:, 1:4], time=left_imu[:, 0], title_name='acc')
+    # imu_plot_aux(left_imu[:, 4:7], time=left_imu[:, 0], title_name='gyr')
+
+    imu_plot_aux(phone_imu[:,1:4],phone_imu[:,0],'acc')
+    imu_plot_aux(phone_imu[:,4:7],phone_imu[:,0],'gyr')
+
 
     plt.show()
