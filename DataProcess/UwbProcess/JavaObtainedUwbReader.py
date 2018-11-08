397dfe92c7aafdf955c21b397c3f9a6b0be8c5d4
steve <551619855@qq.com>
2018-11-08 09:00:45 +0800

    update uwb data preprocess generate UWB signal strength file.(uwb_strength_data.csv)

--- DataProcess/UwbProcess/JavaObtainedUwbReader.py
+++ DataProcess/UwbProcess/JavaObtainedUwbReader.py
@@ -108,6 +108,7 @@ if __name__ == '__main__':
     #                         dir_name + 'beaconSet.csv')
 
     dir_name = "/home/steve/Data/NewFusingLocationData/0048/"
+    dir_name = "/home/steve/Data/ZUPTPDR/0007/"
     uwb_file_p = UwbProcess(dir_name + "HEAD_UWB.data",
                             dir_name + '../BeaconSet.csv')
 
