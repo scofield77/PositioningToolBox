2246c900b9ae493190c4d3f7e94adb4d5c9ff370
steve <551619855@qq.com>
2019-02-14 00:04:35 +0800

    modified data file.

--- PDRTool/PDRLearning/DataLoader/DeepIODataset.py
+++ PDRTool/PDRLearning/DataLoader/DeepIODataset.py
@@ -23,3 +23,15 @@
          佛祖保佑       永无BUG 
 '''
 
+import json
+import torch
+import torch.utils.data as data
+
+import numpy as np
+
+class DeepIODataset(data.Dataset):
+    def __init__(self,dir_name):
+        '''
+        load file name
+        :param dir_name:
+        '''
