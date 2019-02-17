4c55075d4ab5d41fe12e3adec6c65f57f273d582
steve <551619855@qq.com>
2019-02-17 20:39:22 +0800

    modified data file.

--- PDRTool/PDRLearning/DataLoader/DeepIODataset.py
+++ PDRTool/PDRLearning/DataLoader/DeepIODataset.py
@@ -57,11 +57,36 @@ class DeepIODataset(data.Dataset):
                     # print('sub dir:', sub_dir)
                     if sub_dir == 'syn':
                         print(current_dir_name+sub_dir)
+                        self.valid_dir_name_list.append(current_dir_name+sub_dir)
 
                     else:
                         dg_search(current_dir_name+sub_dir)
 
+        # save all the sub directory named 'syn'
         dg_search(dir_name)
+
+    def load_to_mem(self):
+        '''
+        Load data to memory.(save as list of dir)
+        :return:
+        '''
+        for dir_name in self.valid_dir_name_list:
+            print(dir_name)
+            if 'vi1.csv' in os.listdir(dir_name):
+                print(os.listdir(dir_name))
+                if dir_name[-1] != '/':
+                    dir_name  = dir_name + '/'
+                for imu_file_name in os.listdir(dir_name):
+                    if 'imu' in imu_file_name:
+                        vi_file_name = imu_file_name.replace('imu','vi')
+                        print(imu_file_name,vi_file_name)
+
+                        vi_data = np.loadtxt(dir_name+vi_file_name,
+                                             delimiter=',')
+                        imu_data = np.loadtxt(dir_name+imu_file_name,
+                                              delimiter=',')
+                        print('imu shape:', imu_data.shape,
+                              'vi shape:', vi_data.shape)
 
 
 if __name__ == '__main__':
@@ -75,3 +100,4 @@ if __name__ == '__main__':
     # for the_name in os.listdir(dir_name):
     # process .
     # print(the_name, os.path.isdir(dir_name+'/'+the_name))
+    DIODataset.load_to_mem()
